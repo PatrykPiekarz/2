519bfac9066b81654412ff755c835e81228a7f28
PatrykPiekarz <Patryk.Konrad.Piekarz@gmail.com>
2016-12-20 10:23:15 +0100

    Update zad2.py

--- zad2.py
+++ zad2.py
@@ -1,8 +1,6 @@
 #wyswietl tabliczke mnozenia
 
 for x in range (1,11):
-    print "%3i" % (x),
-    for y in range (2,11):
-        if x == 1: print "%3i" % (x*y),
+    for y in range (1,11):
         print "%3i" % (x*y),
     print "\n"
